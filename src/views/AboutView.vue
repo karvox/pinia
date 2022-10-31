186bf136298b2a0a19bf7f19ac3f96daaec6b6a3
Judson Borges <judsonborges@ect.ufrn.br>
2022-10-31 17:57:17 -0300

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <div class="about">
+      <h1>About</h1>
+    </div>
+  </main>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="home">
+     <div class="count">
+      {{ count }}
+     </div>
+     <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+     </div>
+     <hr>
+     This count is: {{ oddOrEven }}
+    </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+
+const increaseCount = () => {
+  count.value++
+}
+
+const decreaseCount = () => {
+  count.value--
+}
+
+const oddOrEven = computed (() => {
+  if (count.value % 2 === 0) return 'even'
+  return 'odd'
+})
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
